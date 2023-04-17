238c64b67db27149900702568d347a812910886f
tossy0130 <tokotoko33ok@gmail.com>
2023-04-17 15:49:32 +0900

    2023_04_17_flask_001

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 import mysql.connector
+import os
+
+app = Flask(__name__)
 
 # === DB 接続情報
 
 """
 
-テスト用テーブル
+テスト用 DB & テーブル
 
-CREATE TABLE ce_table_01 (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    hinmoku VARCHAR(50),
-    nedan INT,
-    zaiko_num INT,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE DATABASE sample_db;
+USE sample_db;
+
+CREATE TABLE users (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(50) NOT NULL,
+email_address VARCHAR(100) NOT NULL,
+PRIMARY KEY (id)
 );
 
-
----------------------------
-
-INSERT INTO ce_table_01
-  (hinmoku, nedan, zaiko_num)
-VALUES 
-  ('hinmoku_01', 100, 30),
-  ('hinmoku_02', 200, 20),
-  ('hinmoku_03', 300, 10);
+use sample_db;
+insert into users (name, email_address) values
+('John Smith', 'john@example.com'),
+('Jane Doe', 'jane@example.com'),
+('Bob Johnson', 'bob@example.com'),
+('Alice Lee', 'alice@example.com'),
+('Mike Davis', 'mike@example.com');
 
 """
 
@@ -32,26 +35,54 @@ def conn_db():
     conn = mysql.connector.connect(
         host='127.0.0.1',
         user='root',
-        password='',
-        db='flask_db_01'
+        password='root',
+        port='33306',
+        # db='flask_db_01'
+        db='sample_db'
     )
 
     return conn
 
 
-# SQL 文
-sql = 'SELECT * FROM ce_table_01'
+@app.route('/select', methods=['GET'])
+# ========= Mysql SELECT
+def select():
 
-try:
-    conn = conn_db()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-except(mysql.connector.errors.ProgrammingError) as e:
-    print('エラー')
-    print(e)
+    # SQL 文
+    sql = 'SELECT * FROM users'
 
-print('select開始')
+    try:
+        conn = conn_db()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+    except(mysql.connector.errors.ProgrammingError) as e:
+        print('エラー')
+        print(e)
 
-for r_val in rows:
-    print(r_val[0], r_val[1], r_val[2], r_val[3])
+    return render_template('select.html', rows=rows)
+
+
+@app.route('/', methods=['GET', 'POST'])
+# ========= Mysql インサート処理
+def up():
+    if request.method == 'POST':
+        # === form の値取得
+        name = request.form['name']
+        email_address = request.form['email_address']
+
+        # === Mysql Insert 処理
+        conn = conn_db()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO users (name, email_address) VALUES (%s, %s)", (name, email_address))
+
+        conn.commit()
+
+        cursor.close()
+        return redirect(url_for('select'))
+    return render_template('up.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
